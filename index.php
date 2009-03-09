bb0c9ec9d5cab98f6ebc015a75a27a3b937bbe86
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-03-09 16:22:07 +0000

    Added exception catch at the bootstrap code to catch some catastrophic issues

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ set_include_path('app/models' . PATH_SEPARATOR . get_include_path());
 set_include_path('app/controls' . PATH_SEPARATOR . get_include_path());  
 require_once "Zend/Loader.php"; 
 Zend_Loader::registerAutoload(); 
-Zend_Session::start();
 
 //load our stuff
 require_once("app/roles.php");
@@ -14,20 +13,28 @@ require_once("config.php");
 require_once("app/views/helper.php");
 require_once("app/base.php");
 
-remove_quotes();
-setup_logs();
-greet();
-cert_authenticate();
+try {
+    Zend_Session::start();
 
-//set php config
-ini_set('error_log', config()->error_logfile);
-ini_set('display_errors', 0); 
-ini_set('log_errors', 1); 
-ini_set('display_startup_errors', 1);  
-error_reporting(E_ALL | E_STRICT);  
-date_default_timezone_set("UTC");
+    remove_quotes();
+    setup_logs();
+    greet();
+    cert_authenticate();
 
-//dispatch
-$frontController = Zend_Controller_Front::getInstance(); 
-$frontController->setControllerDirectory('app/controls'); 
-$frontController->dispatch(); 
+    //set php config
+    ini_set('error_log', config()->error_logfile);
+    ini_set('display_errors', 0); 
+    ini_set('log_errors', 1); 
+    ini_set('display_startup_errors', 1);  
+    error_reporting(E_ALL | E_STRICT);  
+    date_default_timezone_set("UTC");
+
+    //dispatch
+    $frontController = Zend_Controller_Front::getInstance(); 
+    $frontController->setControllerDirectory('app/controls'); 
+    $frontController->dispatch(); 
+} catch(exception $e) {
+    //when a catastrohpic failure occure (like disk goes read-only..) emailing is the only way we got..
+    mail(config()->elog_email_address, "[gocticket] caught unhandled exception ", $e, "From: ".config()->email_from);
+    print "unhandled exception";
+}

f43af1a0ae40fbaa9945edb7fd98d244befe15fa
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-03-23 20:05:23 +0000

    Logic error(2)

--- app/views/helper.php
+++ app/views/helper.php
@@ -155,7 +155,7 @@ function nadstyle($nad)
     $nad = strtotime($nad);
     if($nad < time()) {
         return "red";
-    } else if($nad - 3600*24*7 < time()) {
+    } else if($nad < time() + 3600*24*7) {
         return "yellow";
     }
     return "";

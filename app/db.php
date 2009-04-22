f94780c4fc2f59ee141eb303b3a0a7622aaef219
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-04-22 18:30:36 +0000

    removed old comment

--- app/db.php
+++ app/db.php
@@ -25,6 +25,7 @@ function connect_db()
 
     $g_db = $db;
 }
+
 function db() { 
     global $g_db; 
     if($g_db == null) {
@@ -33,33 +34,3 @@ function db() {
     return $g_db; 
 }
 
-/*
-function dump_db_profile()
-{
-    $out = "";
-
-    if(Zend_Registry::isRegistered('db')) {
-        $db = Zend_Registry::get('db');
-        $profiler = $db->getProfiler();
-
-        $totalTime    = round($profiler->getTotalElapsedSecs(), 2);
-        $queryCount   = $profiler->getTotalNumQueries();
-        $longestTime  = 0;
-        $longestQuery = null;
-
-        if($profiler->getQueryProfiles()) {
-            $out .= "DB PROFILE ----------------------------------------------------------------------\n";
-            $out .= "Executed $queryCount queries in $totalTime seconds.\n";
-
-            foreach ($profiler->getQueryProfiles() as $query) {
-                if ($query->getElapsedSecs() > $longestTime) {
-                    $longestTime  = $query->getElapsedSecs();
-                    $longestQuery = $query->getQuery();
-                }
-                $out .= "[".round($query->getElapsedSecs(),2)." seconds]\n\t".$query->getQuery()."\n";
-            }
-        }
-    }
-    return $out;
-}
-*/

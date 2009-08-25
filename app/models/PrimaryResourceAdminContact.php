c3d7c5509ac4c469ff70da70bd5f35bd89c51417
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-08-25 19:04:13 +0000

    Removed "active = 1" where clause from contact table related queries

--- app/models/PrimaryResourceAdminContact.php
+++ app/models/PrimaryResourceAdminContact.php
@@ -4,7 +4,7 @@ class PrimaryResourceAdminContact
 {
     public function fetch($resource_id)
     {
-        $sql = "select * from contact where id = (SELECT contact_id from resource_contact where contact_type_id = 3 and contact_rank_id = 1 and resource_id = $resource_id)";
+        $sql = "select * from contact where id = (SELECT contact_id from resource_contact where contact_type_id = 3 and contact_rank_id = 1 and resource_id = $resource_id) and disable = 0";
         return db2()->fetchRow($sql);
     }
 }

aff6a6872c3ba7d92ed36b1ec42f7283cdd8ce7e
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-11-03 22:37:46 +0000

    added timeout for fpcall and add trap for connection issue due to n/w

--- app/base.php
+++ app/base.php
@@ -89,14 +89,19 @@ function signedmail($to, $from, $subject, $body, $header = "")
 
 function fpcall($function, $param)
 {
-    //passing connection_timeout doesn't seem to work in case of host not routable.. maybe a different timeout applies?
-    $client = new SoapClient(null, array('location' => config()->fp_soap_location, 'uri' => config()->fp_soap_uri));
+    $client = new SoapClient(null, array('location' => config()->fp_soap_location, 'uri' => config()->fp_soap_uri, 'connection_timeout'=>5));
     for($i = 0; $i < 5; $i++) {
         try {
             $ret = $client->__soapCall($function, $param);
             return $ret;
         } catch (SoapFault $e) {
-            elog("fpcall: SoapFault -- ".$e->getMessage());
+            $msg = $e->getMessage();
+            if($msg == "Could not connect to host") {
+                //this happens when server is gone - due to like n/w issue -- bail!
+                elog("fpcall: SoapFault (bailing) -- ".$msg);
+                throw new exception("Underlying system that this application depends on is having an issue : ".$msg);
+            } 
+            elog("fpcall: SoapFault (trying again) -- ".$msg);
         }
     }
     elog("Soap called failed too many times.. quitting");

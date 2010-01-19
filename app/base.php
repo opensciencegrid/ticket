759abcf5b71c8d73947c47968b2de1b7cc24d4e1
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-01-19 18:10:18 +0000

    Added feature to go directly to the specified update.

--- app/base.php
+++ app/base.php
@@ -115,3 +115,10 @@ function fpcall($function, $param)
     elog("Soap called failed too many times.. quitting");
     throw new exception("Couldn't contact IU Footprint server : ".$msg);
 }
+
+function selfURL() { 
+    $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : ""; $protocol = strleft(strtolower($_SERVER["SERVER_PROTOCOL"]), "/").$s; $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]); return $protocol."://".$_SERVER['SERVER_NAME'].$port.$_SERVER['REQUEST_URI']; 
+} 
+function strleft($s1, $s2) { 
+    return substr($s1, 0, strpos($s1, $s2)); 
+}

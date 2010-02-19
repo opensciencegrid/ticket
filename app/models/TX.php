033e387a7f6575457901589f061b96c09b50ef56
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-02-19 02:25:43 +0000

    updated to include fp71 tx information for tx name lookup.

--- app/models/TX.php
+++ app/models/TX.php
@@ -9,7 +9,7 @@ class TX
         $sql = "select * from sync where source_id = $id and tx_id like 'fp%'";
         foreach(txdb()->fetchAll($sql) as $row) {
             $txid = $row->tx_id;
-            $txid = substr($txid, strpos($txid, "_")+1);
+            //$txid = substr($txid, strpos($txid, "_")+1);
             $links[$txid] = $row->dest_id;
         }
         return $links;

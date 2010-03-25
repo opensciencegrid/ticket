420d5a6e59c9fe51218a009db70965aee62b1888
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-03-25 13:14:25 +0000

    added wlog function and used it for unknown TXID issue

--- app/log.php
+++ app/log.php
@@ -58,7 +58,22 @@ function elog($obj)
     // 0) message is sent to PHP's system logger, using the Operating System's 
     // system logging mechanism or a file, depending on what the error_log  
     // configuration directive is set to. This is the default option. 
-    error_log($obj, 0); 
+    error_log("[ERR] ".$obj, 0); 
+}
+
+//warning log
+function wlog($obj)
+{
+    if(is_string($obj)) {
+        $obj = log_format($obj);
+    } 
+    Zend_Registry::get("logger")->log($obj, Zend_Log::WARN);
+
+    //send to error_log as well
+    // 0) message is sent to PHP's system logger, using the Operating System's 
+    // system logging mechanism or a file, depending on what the error_log  
+    // configuration directive is set to. This is the default option. 
+    error_log("[WARN] ".$obj, 0); 
 }
 
 //standard log

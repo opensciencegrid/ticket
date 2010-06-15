0f9e24933fda892d47ad4a512ee956a20b487dfa
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-06-15 14:57:21 +0000

    Made navigator2 production and renamed original navigator to navigatorold

--- app/controls/NavigatorController.php
+++ app/controls/NavigatorController.php
@@ -1,158 +1,56 @@
 <?
 
+require_once("app/json.php");
+
 class NavigatorController extends Zend_Controller_Action 
 { 
     public function init()
     {
         $this->view->submenu_selected = "view";
-
-        $this->view->sortby = "nad";
-        if(isset($_REQUEST["sortby"])) {
-            //dlog("settin sortby to ".$_REQUEST["sortby"]);
-            $this->view->sortby = $_REQUEST["sortby"];
-        }
-
-        $this->view->sortdir = "up";
-        if(isset($_REQUEST["sortdir"])) {
-            $this->view->sortdir = $_REQUEST["sortdir"];
-        }
-
+        $this->view->page_title = "Ticket List";
     }
 
     public function indexAction()
     {
-        if(user()->allows("admin")) {
-            $this->openassignAction();
-        } else {
-            $this->openoriginAction();
-        }
-    }
-
-    public function openAction()
-    {
-        try {
-            $model = new Tickets();
-            $tickets = $model->getopen();
-            $this->view->activetab = "open";
-            $this->view->tickets = $this->groupby("mrdest", $tickets);
-            $this->render("index");
-        } catch (SoapFault $e) {
-            elog("SoapFault detected while ViewController:loaddetail()");
-            elog($e->getMessage());
-            $this->view->content = $e->getMessage();
-            $this->render("error/error", null, true);
-        }
-    }
-    public function openoriginAction()
-    {
-        try {
-            $model = new Tickets();
-            $tickets = $model->getopen();
-            $this->view->activetab = "openorigin";
-            $this->view->tickets = $this->groupby("mrorigin", $tickets);
-            $this->render("index");
-        } catch (SoapFault $e) {
-            elog("SoapFault detected while ViewController:loaddetail()");
-            elog($e->getMessage());
-            $this->view->content = $e->getMessage();
-            $this->render("error/error", null, true);
-        }
-    }
-    public function openassignAction()
-    {
-        try {
-            $model = new Tickets();
-            $tickets = $model->getopen();
-            $this->view->activetab = "openassign";
-            $this->view->tickets  = $this->groupby("mrassignees", $tickets);
-            $this->render("index");
-        } catch (SoapFault $e) {
-            elog("SoapFault detected while ViewController:loaddetail()");
-            elog($e->getMessage());
-            $this->view->content = $e->getMessage();
-            $this->render("error/error", null, true);
-        }
-    }
-    public function closeAction()
-    {
-        try {
-            $model = new Tickets();
-            $tickets = $model->getclosed(config()->closeticket_window);
-            $this->view->activetab = "close";
-            $this->view->tickets  = $this->groupby("mrdest", $tickets);
-            $this->render("index");
-        } catch (SoapFault $e) {
-            elog("SoapFault detected while ViewController:loaddetail()");
-            elog($e->getMessage());
-            $this->view->content = $e->getMessage();
-            $this->render("error/error", null, true);
-        }
-    }
-    public function closeoriginAction()
-    {
-        try {
-            $model = new Tickets();
-            $tickets = $model->getclosed(config()->closeticket_window);
-            $this->view->activetab = "closeorigin";
-            $this->view->tickets  = $this->groupby("mrorigin", $tickets);
-            $this->render("index");
-        } catch (SoapFault $e) {
-            elog("SoapFault detected while ViewController:loaddetail()");
-            elog($e->getMessage());
-            $this->view->content = $e->getMessage();
-            $this->render("error/error", null, true);
-        }
-    }
-
-    protected function groupby($type, $tickets) 
-    {
-        $aka_model = new AKA();
-        
-        $schema_model = new Schema();
-        $teams = $schema_model->getteams();
-
-        //group tickets by certain field id
-        $grouped_tickets = array();
-        foreach($tickets as $ticket) {
-            $token = Footprint::parse($ticket->$type);
-            if($type == "mrassignees") {
-                //assignees are wierd (need to filter by certain team)
-
-                $fp_goc = array();
-                foreach($teams as $id=>$team) {
-                    if(in_array($id, config()->navigator_assignee_list)) {
-                        $fp_goc = array_merge($fp_goc, split(",", $team->members));
-                    }
-                }
-
-                foreach(split(" ", $token) as $a) {
-                    if(strlen($a) >= 3 and strpos($a, "CC:") === 0) {
-                        //ignore CCs
-                        continue;
-                    }
-
-                    //ignore non-goc assignee
-                    if(!in_array($a, $fp_goc)) {
-                        continue;
-                    }
-
-                    //lookup real name
-                    $aka = $aka_model->lookupName($a);
-                    if($aka !== null) $a = $aka;
-
-                    //ignore non username
-                    if(!isset($grouped_tickets[$a])) {
-                        $grouped_tickets[$a] = array();
-                    }
-                    $grouped_tickets[$a][] = $ticket;
-                }
-            } else {
-                if(!isset($grouped_tickets[$token])) {
-                    $grouped_tickets[$token] = array();
-                }
-                $grouped_tickets[$token][] = $ticket;
+        //TODO - this is a hack until DataTable provides a way to query column visibility information natively
+        //find cookie for datatables
+        $this->view->opened_table_cols = array();
+        $this->view->closed_table_cols = array();
+        foreach($_COOKIE as $key=>$c) {
+            if(strpos($key, "SpryMedia_DataTables_opened") === 0){
+                $json = json_decode($c);
+                $this->view->opened_table_cols = $json->abVisCols;
+            } else if(strpos($key, "SpryMedia_DataTables_closed") === 0){
+                $json = json_decode($c);
+                $this->view->closed_table_cols = $json->abVisCols;
             }
         }
-        return $grouped_tickets;
+
+        try {
+            $model = new Tickets();
+            $this->view->page_title = "Open Tickets";
+
+            //assigned tickets
+            $closed_status = "('Closed', '_DELETED_', '_SOLVED_', 'Resolved')";
+            $query = "WHERE mrstatus not in $closed_status";
+            $this->view->assigned_tickets = $model->dosearch($query);
+
+            //recently closed tickets
+            $this->view->closed_days = config()->myticket_closed_days;
+            $recent_date = date("Y-m-d G:i:s", time()-3600*24*$this->view->closed_days);
+
+            $query = "WHERE mrstatus in $closed_status and mrupdatedate > '$recent_date'";
+            $this->view->closed_tickets = $model->dosearch($query);
+
+            $model = new Schema();
+            $this->view->teams = $model->getteams();
+
+        } catch (SoapFault $e) {
+            elog("SoapFault detected while ViewController:loaddetail()");
+            elog($e->getMessage());
+            $this->view->content = $e->getMessage();
+            $this->render("error/error", null, true);
+        }
     }
+
 } 

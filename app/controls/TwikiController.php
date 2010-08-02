3e2591012fe08937e7f5aaccda7ad75f51e241cc
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-08-02 18:29:46 +0000

    Added an email filter for guest user [GOCTICKET-64] Fixed the layout issue for guest user on ticket viewer. Added a new field for twiki page [GOCTICKET-68]

--- app/controls/TwikiController.php
+++ app/controls/TwikiController.php
@@ -9,9 +9,25 @@ class TwikiController extends GOCServiceController
     }
 
     function addElements($form) {
-        //nothing to add
+        $e = new Zend_Form_Element_Radio("type");
+        $e->setLabel("Issue Regarding")
+        ->addMultiOptions(array(
+            "bug" => "Bugs or Outage on TWiki itself",
+            "content" => "Content or Account",
+        ));
+        $e->setRequired(true);
+
+        $form->addElement($e);
+
     }
     function processFields($form, $footprint) {
-        //nothing to pull
+        if($form->getValue('type') == "content") {
+            //Assign GOC support
+            $model = new NextAssignee();
+            $gocid = $model->getNextAssignee();
+            $footprint->addAssignee($gocid, true); //reset assignee first
+        } else {
+            //by default. all GOC Service ticket goes to infrastructure
+        }
     }
 } 

60c34c3832967aa9388619b814b41303150fbab9
/C=US/ST=Indiana/L=Bloomington/O=Open Science Grid/OU=Grid Operations Center/CN=*.grid.iu.edu/svn/emailAddress=goc@opensciencegrid.org </C=US/ST=Indiana/L=Bloomington/O=Open Science Grid/OU=Grid Operations Center/CN=*.grid.iu.edu/svn/emailAddress=goc@opensciencegrid.org@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2013-02-12 21:48:09 +0000

    removed e again

--- app/models/NextAssignee.php
+++ app/models/NextAssignee.php
@@ -16,19 +16,8 @@ class NextAssignee
         $weekday = $time["tm_wday"];
 
         //construct list of possible assignee based on each hours
+
 /*
-        if($hour >= 5 and $hour < 8) {
-            $members = array("adeximo");
-        } else if ($hour >= 8 and $hour < 13) {
-            $members = array("adeximo", "echism", "kagross");
-        } else if ($hour >= 13 and $hour < 17) {
-            $members = array("adeximo", "echism", "kagross", "cpipes");
-        } else if ($hour >= 17 and $hour < 21) {
-            $members = array("cpipes");
-        } else {
-            $members = array("adeximo", "echism", "kagross");
-        }
-*/
         if ($hour >= 8 and $hour < 13) {
             $members = array("echism", "kagross");
         } else if ($hour >= 13 and $hour < 17) {
@@ -37,6 +26,16 @@ class NextAssignee
             $members = array("cpipes");
         } else {
             $members = array("echism", "kagross");
+        }
+*/
+        if ($hour >= 8 and $hour < 13) {
+            $members = array("kagross");
+        } else if ($hour >= 13 and $hour < 17) {
+            $members = array("kagross", "cpipes");
+        } else if ($hour >= 17 and $hour < 21) {
+            $members = array("cpipes");
+        } else {
+            $members = array("kagross");
         }
 
         //report the pool of possible staff

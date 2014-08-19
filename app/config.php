071b84aa7cf0bc3a953376b5c309a677ea9013b1
Soichi Hayashi <soichih@gmail.com>
2014-08-19 17:28:49 +0000

    1.80 begins

--- app/config.php
+++ app/config.php
@@ -14,7 +14,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "1.79";
+        $this->version = "1.80";
         //application to display or used in email
         $this->app_name = "GOC Ticket";
         //application name used in places such as session name

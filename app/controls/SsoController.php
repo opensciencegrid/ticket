1b6adb1f6d7ae1131cda956080d230ad12583be8
root <root@ticket-dev.grid.iu.edu>
2017-10-04 20:28:48 +0000

    added vendor files

---
+++ app/controls/SsoController.php
@@ -0,0 +1,13 @@
+<?
+
+class SsoController extends Zend_Controller_Action 
+{ 
+    public function indexAction() 
+    { 
+      // $response = $this->getResponse();
+	//   $response->setRedirect(fullbase()."/sso");
+      //  $this->render();
+
+
+    }
+} 
